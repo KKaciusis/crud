4d0033c906f2faba979fc635501f7c164cab7b3e
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-30 21:02:18 +0300

    connected to db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 const express = require('express');
-
 const app = express();
+const mysql = require('mysql');
+
+const db = mysql.createPool({
+    host: 'localhost',
+    database: 'cowdb',
+    user: 'Bloten',
+    password: 'milkexpert3000',
+})
+
+app.get('/', (req, res) =>{
+
+    const sqlinsert = "INSERT INTO cow_tier (cowName, favoriteSnack, milkProduction) VALUES ('cowalingus', 'carotis', '35');"
+    db.query(sqlinsert, (err, result) => {
+        res.send('hello nigeriao');
+    });
+});
 
 app.listen(3001, () => {
     console.log('runin');
