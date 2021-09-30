0c16eebc33814a0dc5f538fa710bd32ecd9f7471
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-09-30 19:45:32 +0300

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="form">
+        <input type="text"></input>
+        <input type="text"></input>
+        <input type="text"></input>
+        <button>submit</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
