138ea419b6e080e027deb369003519d0bd95bd02
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-10-01 16:03:23 +0300

    working connection with db , working db  push

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-const bodyParser = require('body-parser');
+const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -11,14 +11,19 @@ const db = mysql.createPool({
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASS,
 });
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.post("/api/insert", (req, res) => {
 
-app.post('/api/insert', (req, res) => {
+    const cowName = req.body.cowName;
+    const favoriteSnack = req.body.favoriteSnack;
+    const milkProduction = req.body.milkProduction;
 
-    const sqlinsert = "INSERT INTO cowdb (cowName, favorteSnack, milkProduction) VALUES (?, ?, ?)";
+    const sqlinsert = "INSERT INTO cow_tier (cowName, favoriteSnack, milkProduction) VALUES (?, ?, ?)";
     db.query(sqlinsert, [cowName, favoriteSnack, milkProduction], (err, result) => {
-
+        console.log(err)
     });
 });
 
