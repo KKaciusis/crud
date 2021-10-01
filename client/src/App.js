98c73012826b7882cda354bdb08302b79a81b97a
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-10-01 15:21:51 +0300

    front

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,12 @@
 import './App.css';
 import React, { useState, useEffect } from "react";
 import Axios from 'axios';
+import Meniu from './components/meniu';
 
 function App() {
-
-  const [cowName, setCowName] = useState('');
-  const [favoriteSnack, setFavoriteSnack] = useState('');
-  const [milkProduction, setMilkProduction] = useState('');
-
-  const submitCow = () => {
-    Axios.post('http://localhost:3001/api/insert', {cowName: cowName, favoriteSnack: favoriteSnack, milkProduction: milkProduction}).then(() => {
-      console.log('insert compleated');
-    });
-  };
-
-  return (
-    <div className="App">
-      <div className="form">
-        <input type="text" name="cowName" onChange={(e) => setCowName(e.target.value)}></input>
-        <input type="text" name="favoriteSnack" onChange={(e) => setFavoriteSnack(e.target.value)}></input>
-        <input type="text" name="milkProduction" onChange={(e) => setMilkProduction(e.target.value)}></input>
-        <button onClick={submitCow}>submit</button>
-      </div>
-    </div>
-  );
+  return(
+    <Meniu/>
+  )
 }
 
 export default App;
---
+++ client/src/components/meniu.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+import Axios from 'axios';
+
+function Meniu() {
+
+    const [cowName, setCowName] = useState('');
+    const [favoriteSnack, setFavoriteSnack] = useState('');
+    const [milkProduction, setMilkProduction] = useState('');
+  
+    const submitCow = () => {
+      Axios.post('http://localhost:3001/api/insert', {cowName: cowName, favoriteSnack: favoriteSnack, milkProduction: milkProduction}).then(() => {
+        console.log('insert compleated');
+      });
+    };
+  
+    return (
+      <div className="App">
+        <div className="form">
+          <input type="text" name="cowName" onChange={(e) => setCowName(e.target.value)}></input>
+          <input type="text" name="favoriteSnack" onChange={(e) => setFavoriteSnack(e.target.value)}></input>
+          <input type="text" name="milkProduction" onChange={(e) => setMilkProduction(e.target.value)}></input>
+          <button onClick={submitCow}>submit</button>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Meniu;
