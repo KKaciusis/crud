735f2facb90cd8df1df98c04bf627cc8fe674d79
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-10-06 15:47:57 +0300

    Admin sits on /admin

--- client/src/Admin.js
+++ client/src/Admin.js
@@ -2,22 +2,17 @@
 import './App.css';
 import './index.css';
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-
 import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 
 import Dashboard from './components/Dashboard';
-import HomePage from './components/HomePage';
 import { authProvider } from './components/authentification/Auth';
-import AdminPage from './Admin';
 
 const dataProvider = jsonServerProvider('http://localhost:3001/api');
-const App = () => (
-    <Router>
-        <Route path="/admin" component={AdminPage}/>
-        <Route path="/" component={HomePage}/>
-    </Router>
+const AdminPage = () => (
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+        <Resource name="cows" list={ListGuesser} />
+    </Admin>
 );
 
-export default App;
+export default AdminPage;
