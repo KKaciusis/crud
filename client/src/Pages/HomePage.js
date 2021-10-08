77d893aa0cb08b1e8820cd109e430fe746792561
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-10-08 18:15:10 +0300

    Footer sticking to the bottom

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,5 +1,6 @@
 import CowsHome from "../components/CowObjects/cowsForHomePage";
 import Footer from "../components/Home/Footer";
+import "../index.css"
 
 export default () => (
     <div class="wrapper">
--- client/src/components/Home/Footer.jsx
+++ client/src/components/Home/Footer.jsx
@@ -1,25 +1,29 @@
 import * as React from 'react';
 import Container from '@material-ui/core/Container';
-import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
-import Link from '@material-ui/core/Link';
 import SupervisorAccountOutlinedIcon from '@material-ui/icons/SupervisorAccountOutlined';
 import ImportContactsIcon from '@material-ui/icons/ImportContacts';
 import Button from '@material-ui/core/Button';
-import { Typography } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+import '../../index.css'
 
 const Footer = () => (
     <footer className="footer">
-    <Box bgcolor="#73839c">
-        <Container maxWidth="lg">
+    <Box bgcolor="#98afd4">
+        <Container>
             <Box display="flex" justifyContent="center">
-                    <Box height="40px" padding="10px" display="flex" alignItems='center' justifyContent='center'>
-                    <Button variant="outlined" href="/contacts" endIcon={<SupervisorAccountOutlinedIcon/>}>Contacts</Button>
-                    </Box>
-                    <Box height="40px" padding="10px" display="flex" alignItems="center" justifyContent="center">
-                    <Button variant="outlined" href="/contacts" endIcon={<ImportContactsIcon/>}>Contacts</Button>
-                    </Box>
+                <Box height="40px" padding="10px" display="flex" alignItems='center' justifyContent='center'>
+                <Button variant="outlined" href="/admin" endIcon={<SupervisorAccountOutlinedIcon/>}>Admin Page</Button>
                 </Box>
+                <Box height="40px" padding="10px" display="flex" alignItems="center" justifyContent="center">
+                <Button variant="outlined" href="/contacts" endIcon={<ImportContactsIcon/>}>Contacts Page</Button>
+                </Box>
+                <IconButton href="https://www.youtube.com/watch?v=9hhMUT2U2L4"><YouTubeIcon/></IconButton>
+            </Box>
+            <Box color="white" textAlign="center">
+                COW TIER LIST &reg; {new Date().getFullYear()}
+            </Box>
         </Container>
     </Box>
     </footer>
