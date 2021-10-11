6db9d7442322db758dc5690bd8f356da4bdd23c3
Kristijonas Kačiušis <84323029+KKaciusis@users.noreply.github.com>
2021-10-11 19:34:19 +0300

    responsive table with delete action

--- client/src/components/cows/CowTable.jsx
+++ client/src/components/cows/CowTable.jsx
@@ -7,45 +7,54 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { useState, useEffect } from "react";
-import Axios from 'axios';
+import axios from 'axios';
+import Button from '@material-ui/core/Button';
 
 
 export default function CowTable() {
-    
-  const [cows, setCows] = useState([]);
+    const [cows, setCows] = useState([]);
 
-  useEffect(()=>{
-    Axios.get('http://localhost:3001/api/cows').then((response) => {
-      setCows(response.data);
-    });
-}, [])
+    useEffect(() => {
+        axios.get('http://localhost:3001/api/cows').then((response) => {
+            setCows(response.data);
+        });
+    }, [])
 
-return (
-    <TableContainer component={Paper}>
-      <Table sx={{ width: '100%' }} size="small" aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Cow Photo Name</TableCell>
-            <TableCell align="right">Cow ID</TableCell>
-            <TableCell align="right">Cow Name</TableCell>
-            <TableCell align="right">Favorire Snack</TableCell>
-            <TableCell align="right">Milk Production</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {cows.map((cow)=>(
-        <TableRow key={cow.cowName} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row">
-                  {cow.imgPath}
-              </TableCell>
-              <TableCell align="right">{cow.id}</TableCell>
-              <TableCell align="right">{cow.cowName}</TableCell>
-              <TableCell align="right">{cow.favoriteSnack}</TableCell>
-              <TableCell align="right">{cow.milkProduction}</TableCell>
-              </TableRow>
-              ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  );
+    const deleteElement = (id) => {
+        if (window.confirm('Y U NO COW?!!?')) {
+            axios.delete('http://localhost:3001/api/cows/' + id);
+            window.location.reload();
+        }
+    }
+
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ width: '100%' }} size="small" aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Cow Photo Name</TableCell>
+                        <TableCell align="right">Cow ID</TableCell>
+                        <TableCell align="right">Cow Name</TableCell>
+                        <TableCell align="right">Favorire Snack</TableCell>
+                        <TableCell align="right">Milk Production</TableCell>
+                        <TableCell align="right">Actions</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {cows.map((cow) => (
+                        <TableRow key={cow.cowName} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                            <TableCell component="th" scope="row">
+                                {cow.imgPath}
+                            </TableCell>
+                            <TableCell align="right">{cow.id}</TableCell>
+                            <TableCell align="right">{cow.cowName}</TableCell>
+                            <TableCell align="right">{cow.favoriteSnack}</TableCell>
+                            <TableCell align="right">{cow.milkProduction}</TableCell>
+                            <TableCell align="right"><Button size="small" onClick={() => deleteElement(cow.id)}>DROP DAT COW</Button></TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
